9cfe6e4bc9b560c7c66a2648b0438cd89e554994
Philip Crawford <hdphilip@yahoo.com>
2021-09-11 21:04:59 -0700

    Update Tag_ssd1351_scroll_1_0.py
    changed udp port to match

--- Tag_ssd1351_scroll_1_0.py
+++ Tag_ssd1351_scroll_1_0.py
@@ -16,7 +16,7 @@ serial = spi(device=0, port=0, gpio=GPIO)
 device = ssd1351(serial)
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 28002
+UDP_PORT = 28003
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
